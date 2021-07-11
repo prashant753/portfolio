520185cd3f48d6c3e889123bbd6f9d83c19cbc21
prashantgrw <prashant.adhikari@groww.in>
2021-07-11 12:45:17 +0530

    Dependency added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BaseProvider, LightTheme } from "baseui";
+import { Provider as StyletronProvider } from "styletron-react";
+import { Client as Styletron } from "styletron-engine-atomic";
 
 import "./index.css";
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,11 +10,14 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./assests/font-awesome/css/all.css";
 
+const engine = new Styletron();
 
 ReactDOM.render(
+  <StyletronProvider value={engine}>
     <BaseProvider theme={LightTheme}>
       <App />
-    </BaseProvider>,
+    </BaseProvider>
+  </StyletronProvider>,
   document.getElementById("root")
 );
 
